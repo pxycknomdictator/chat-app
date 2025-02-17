cfa9248f527c02f8af369f044311f7ad08e25130
Noman <pxycknomdictator@outlook.com>
2025-02-17 10:09:03 +0500

    added authentication api routes

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,10 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+import authRouter from "./routes/user.routes.js";
+
+app.use("/api/auth", authRouter);
+
 app.use(errorHandler);
 
 export { app };
---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register", (req, res) => {});
+router.post("/login", (req, res) => {});
+router.get("/logout", (req, res) => {});
+router.get("/profile", (req, res) => {});
+router.post("/refresh-token", (req, res) => {});
+
+export default router;
