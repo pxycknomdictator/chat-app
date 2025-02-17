218d5e98be345f879fe78347d8bde514d98cdac8
Noman <pxycknomdictator@outlook.com>
2025-02-17 09:17:50 +0500

    added express middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import { config } from "./lib/config.js";
 
 const app = express();
+const corsOptions = { origin: config.ORIGIN, credentials: true };
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 export { app };
