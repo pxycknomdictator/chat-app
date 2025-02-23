cbc76c7c0489a1a1b8accd394876752dacf2b232
Noman <pxycknomdictator@outlook.com>
2025-02-23 20:11:18 +0500

    added routes in frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,23 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Register = () => <h1>Register</h1>;
+const Login = () => <h1>Login</h1>;
+
 export const App = () => {
-  return <div className="text-4xl text-center underline">Chat App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>,
+    ),
+  );
+  return <RouterProvider router={router} />;
 };
