297426165a16f41279da14de87eda81518051d5b
Noman <pxycknomdictator@outlook.com>
2025-02-25 00:36:38 +0500

    increased password length

--- server/src/lib/password.ts
+++ server/src/lib/password.ts
@@ -2,7 +2,7 @@ import * as argon2 from "argon2";
 
 export const hashPassword = async (password: string) => {
   try {
-    return await argon2.hash(password, { hashLength: 20 });
+    return await argon2.hash(password, { hashLength: 30 });
   } catch (error) {
     throw new Error(`Failed to hash password ${error}`);
   }
