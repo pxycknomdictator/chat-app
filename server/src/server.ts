ef45aa8925d7279b593ff090fcc5a41546fc17a4
Noman <pxycknomdictator@outlook.com>
2025-02-26 17:02:09 +0500

    added socket io server integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,27 @@
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 import { app } from "./app.js";
 import { configurations } from "./config/config.js";
 import { database } from "./lib/db.js";
 
+const sockets = new Map();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: configurations.ORIGIN, credentials: true },
+});
+
+io.on("connection", (socket) => {
+  console.log(`New Socket connected`);
+});
+
 (async () => {
   await database();
 
   const PORT = configurations.PORT ?? 9000;
-  const server = createServer(app);
 
   server.listen(PORT, () =>
-    console.log(`server is listening at: http://localhost:${PORT} 🔥`)
+    console.log(`server is listening at: http://localhost:${PORT} 🔥`),
   );
 })();
